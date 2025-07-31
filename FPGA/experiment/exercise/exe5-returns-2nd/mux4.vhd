e9d4f59aff59e35db2f0af402940565512e2d680
Shinichi Yoshinaga <shinichi.yoshinaga@brother.co.jp>
2025-07-31 14:55:10 +0900

    up

---
+++ FPGA/experiment/exercise/exe5-returns-2nd/mux4.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity mux4 is
+	Port ( a: in std_logic_vector(3 downto 0);
+	       b: in std_logic_vector(3 downto 0);
+	       sel: in std_logic;
+	       y: out std_logic_vector (3 downto 0));
+end mux4;
+
+architecture Behavioral of mux4 is
+begin
+	process(a, b, sel)
+	begin
+		case sel is
+			when '0' => y <= a;
+			when '1' => y <= b;
+			when others => y <=(others=>'0');
+		end case;
+	end process;
+end Behavioral;
