496efc32a6ca594370b937046827441cea377191
Shinichi Yoshinaga <shinichi.yoshinaga@brother.co.jp>
2025-08-23 13:33:33 +0900

    up

--- FPGA/experiment/exercise/exe2/compare_v2.vhd
+++ FPGA/experiment/exercise/exe2/compare_v2.vhd
@@ -4,7 +4,30 @@ use ieee.std_logic_unsigned.all;
 
 entity compare is
 	Port(
-	    
+	    da : in std_logic_vector(7 downto 0);
+	    db : in std_logic_vector(7 downto 0);
+	    equ : out std_logic;
+	    agb : out std_logic;
+	    alb : out std_logic
 	);
 end compare;
 
+architecture Behavioral of compare is
+begin
+	process(da, db)
+	begin
+		if  da = db then
+			equ <= '1';
+			agb <= '0';
+			alb <= '0';
+		elsif da > db then
+			equ <= '0';
+			agb <= '1';
+			alb <= '0';
+		else
+			equ <= '0';
+			agb <= '0';
+			alb <= '1';
+		end if;
+	end process;
+end Behavioral;
