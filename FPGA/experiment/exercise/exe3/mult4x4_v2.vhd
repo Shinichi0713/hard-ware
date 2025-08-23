5837fd966c2f8f097d90466045adf1bcffbcf54a
Shinichi Yoshinaga <shinichi.yoshinaga@brother.co.jp>
2025-08-23 18:27:12 +0900

    up

--- FPGA/experiment/exercise/exe3/mult4x4_v2.vhd
+++ FPGA/experiment/exercise/exe3/mult4x4_v2.vhd
@@ -2,15 +2,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-enetity mult4x4 is
+entity mult4x4 is
 	Port(
-		a: in std_logic_vector(3 downnto 0);
-		b: in std_logic_vector(3 downnto 0);
-		product: out std_logic_vector(7 downnto 0)
+		a: in std_logic_vector(3 downto 0);
+		b: in std_logic_vector(3 downto 0);
+		product: out std_logic_vector(7 downto 0)
 	);
 end entity;
 
-archtecture Behavioral of mult4x4 is
+architecture Behavioral of mult4x4 is
 	begin
 		process(a, b)
 		begin
