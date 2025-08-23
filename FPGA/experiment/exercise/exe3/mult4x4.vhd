d63f32898f1f070d17f0a48473782ddbcc116732
Shinichi Yoshinaga <shinichi.yoshinaga@brother.co.jp>
2025-08-23 18:18:31 +0900

    up

--- FPGA/experiment/exercise/exe3/mult4x4.vhd
+++ FPGA/experiment/exercise/exe3/mult4x4.vhd
@@ -1,17 +1,18 @@
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity mult4x4 is
-    Port ( a : in STD_LOGIC_VECTOR (3 downto 0);
-           b : in STD_LOGIC_VECTOR (3 downto 0);
-           product : out STD_LOGIC_VECTOR (7 downto 0));
+	Port ( a : in std_logic_vector (3 downto 0);
+	       b : in std_logic_vector (3 downto 0);
+	       product : out std_logic_vector (7 downto 0));
 end mult4x4;
 
 architecture Behavioral of mult4x4 is
 begin
-    process(a, b)
-    begin
-        product <= std_logic_vector(unsigned(a) * unsigned(b));
-    end process;
+	process(a, b)
+	begin
+		product <= std_logic_vector(unsigned(a) * unsigned(b));
+	end process;
 end Behavioral;
